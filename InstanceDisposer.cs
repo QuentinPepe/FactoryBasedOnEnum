f0e39e6c391aad85846b1287321d6478f5b9879d
Quentin PEPE <quentin.pepe@ensiie.eu>
2024-03-19 13:39:34 +0100

    Add InstanceDisposer

---
+++ InstanceDisposer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FactoryBasedOnEnum
+{
+    public class InstanceDisposer<TEnum, TInterface>
+        where TEnum : Enum
+        where TInterface : class
+    {
+        private static void DisposeInstances(IEnumerable<Lazy<TInterface>> instances)
+        {
+            foreach (TInterface instance in instances.Select(lazy => lazy.Value))
+            {
+                if (instance is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+            }
+        }
+
+        public void DisposeInstances(IReadOnlyDictionary<TEnum, Lazy<TInterface>> instances)
+        {
+            DisposeInstances(instances.Values);
+        }
+    }
+}
